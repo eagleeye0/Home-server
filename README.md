819862c822e731a06847ab81fa392e038211333a
Akshat Jain <akshatjain@gofynd.com>
2024-01-14 14:43:16 +0530

    Added command for services untested

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Home-server
+This server hosts following services:
+FTP server
+http://localhost:21/
+
+Deluge torrent client
+http://localhost:8112/
+
+Jellyfin media server
+http://localhost:8096/
+
+SSH tunnel to this server
+localhost:8081
---
+++ script.sh
@@ -0,0 +1,24 @@
+# Install docker
+sudo apt-get update
+sudo apt install docker.io -y
+# FTP server
+sudo apt install vsftpd
+sudo systemctl start vsftpd
+sudo systemctl enable vsftpd
+sudo useradd -m akshat_ftp
+sudo passwd akshat_ftp
+sudo mkdir /srv/alldata
+sudo usermod -d /srv/alldata akshat_ftp
+sudo systemctl restart vsftpd.service
+# Jellyfin
+docker pull jellyfin/jellyfin
+mkdir /srv/alldata/jellyfin/config
+mkdir /srv/alldata/jellyfin/cache
+mkdir /srv/alldata/jellyfin/media
+# Torrent server
+docker pull linuxserver/deluge
+mkdir /srv/alldata/deluge/config
+# Image host
+
+docker-compose up
+# ssh
