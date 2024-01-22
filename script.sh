2e0e7d02a7437895b81e9eb3ef1049fb14ec2ac5
Akshat Jain <akshatjain@gofynd.com>
2024-01-22 13:00:01 +0530

    fix

--- script.sh
+++ script.sh
@@ -5,6 +5,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 # FTP server
 sudo apt install vsftpd
 sudo systemctl start vsftpd
@@ -14,14 +16,20 @@ sudo passwd akshat_ftp
 # save password here for user
 sudo mkdir /srv/alldata
 sudo usermod -d /srv/alldata akshat_ftp
+sudo chmod a+w /srv/alldata
+sudo su
+echo "enable_write" >> /etc/vsftpd.conf
+su ubuntu
 sudo systemctl restart vsftpd.service
 # Jellyfin
-docker pull jellyfin/jellyfin
+sudo docker pull jellyfin/jellyfin
+mkdir /srv/alldata/jellyfin
 mkdir /srv/alldata/jellyfin/config
 mkdir /srv/alldata/jellyfin/cache
 mkdir /srv/alldata/jellyfin/media
 # Torrent server
-docker pull linuxserver/deluge
+sudo docker pull linuxserver/deluge
+mkdir /srv/alldata/deluge
 mkdir /srv/alldata/deluge/config
 # Image host
 
